f3fb1d2b069bed4b7dd657cab1c4c9c0d9cea55c
DonnieNgK <donniengk@gmail.com>
2025-06-21 16:30:17 -0400

    Test 4 & 5 added and tested.

--- tests/junior/junior.test.ts
+++ tests/junior/junior.test.ts
@@ -23,6 +23,40 @@ test.describe('Junior-Level Tests', () => {
     await page.click('#login-button');
     await expect(page.locator('[data-test="error"]')).toHaveText(/is required/);
     
+  });
+
+  test('4. Add product to cart from inventory', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    await expect(page).toHaveURL(/inventory\.html$/);
+    await expect(page.locator('.inventory_list')).toBeVisible();
+    await expect(page.locator('[data-test="item-4-title-link"]')).toHaveText(/Backpack/);
+    await expect(page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')).toHaveText(/to cart/);
+    await page.click('[data-test="add-to-cart-sauce-labs-backpack"]');
+    await expect(page.locator('[data-test="remove-sauce-labs-backpack"]')).toHaveText(/remove/i);
+    await expect(page.locator('.shopping_cart_container .shopping-cart-badge')).toBeVisible;
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText(/1/);
+    
+  });
+
+  test('5. Remove product from cart on listing page', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    await expect(page).toHaveURL(/inventory\.html$/);
+    await expect(page.locator('.inventory_list')).toBeVisible();
+    await expect(page.locator('[data-test="item-4-title-link"]')).toHaveText(/Backpack/);
+    await expect(page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')).toHaveText(/to cart/);
+    await page.click('[data-test="add-to-cart-sauce-labs-backpack"]');
+    await expect(page.locator('[data-test="remove-sauce-labs-backpack"]')).toHaveText(/remove/i);
+    await expect(page.locator('.shopping_cart_container .shopping-cart-badge')).toBeVisible;
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText(/1/);
+    await page.click('[data-test="remove-sauce-labs-backpack"]');
+    await expect(page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')).toHaveText(/to cart/);
+    await expect(page.locator('.shopping_cart_container .shopping-cart-badge')).not.toBeVisible;
     
   });
 });
